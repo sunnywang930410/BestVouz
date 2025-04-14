6a6d679552fc0c7e1031bde334f8c7f01bb264a5
AMA0099112 <s111119022@stu.ntue.edu.tw>
2025-04-15 02:37:34 +0800

    修改Header 新增RWD
    shadow還沒有做出來

--- src/component/HamMenu.jsx
+++ src/component/HamMenu.jsx
@@ -1,30 +1,27 @@
-function HamMenu({ onClick, mobileMenuOpen, className }) {
+function HamMenu({ onClick, isOpen, className }) {
     return (
         <span
             onClick={onClick}
-            className={`inline-block absolute z-10 w-16 h-10 opacity-70 left-16 top-8 hover:opacity-100 md:hidden flex items-center justify-center ${className}`}
+            className={`inline-flex w-8 h-10 items-center justify-center opacity-70 hover:opacity-100 md:hidden ${className}`}
         >
             <span className="w-8 h-6 flex flex-col justify-between hover:cursor-pointer">
                 {/* 第一條線 */}
                 <span
-                    className={`bg-base-content h-[3px] transition-all duration-400 w-full origin-center ${mobileMenuOpen
+                    className={`bg-base-content h-[3px] transition-all duration-400 w-full origin-center ${isOpen
                         ? "opacity-0"
-                        : "translate-y-[6px] rotate-[-4deg]" /* 修正旋轉前的位置 */
-                        }`}
+                        : "translate-y-[6px] rotate-[-4deg]"}`}
                 ></span>
                 {/* 第二條線 */}
                 <span
-                    className={`bg-base-content h-[3px] w-full transition-all duration-400 origin-center ${mobileMenuOpen
-                        ? "rotate-45 w-8 translate-y-[5px]" /* 調整旋轉軸心 */
-                        : "translate-y-[3px] rotate-[2deg]"
-                        }`}
+                    className={`bg-base-content h-[3px] w-full transition-all duration-400 origin-center ${isOpen
+                        ? "rotate-45 w-8 translate-y-[5px]"
+                        : "translate-y-[3px] rotate-[2deg]"}`}
                 ></span>
                 {/* 第三條線 */}
                 <span
-                    className={`bg-base-content h-[3px] w-full transition-all duration-400 origin-center ${mobileMenuOpen
-                        ? "rotate-[-45deg] w-8 -translate-y-[6px]" /* 修正旋轉軸心 */
-                        : "rotate-[-2deg]"
-                        }`}
+                    className={`bg-base-content h-[3px] w-full transition-all duration-400 origin-center ${isOpen
+                        ? "rotate-[-45deg] w-8 -translate-y-[6px]"
+                        : "rotate-[-2deg]"}`}
                 ></span>
             </span>
         </span>
---
+++ src/component/NavBar.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react"
+import { Link, NavLink } from "react-router";
+import HamMenu from "./HamMenu";
+import { CircleUserRound, ShoppingCart } from "lucide-react";
+
+const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const navBarContent = [
+        { to: "/", label: "首頁" },
+        { to: "/menu", label: "菜單" },
+        { to: "/about", label: "關於我們" },
+    ]
+
+    const NavBarContent = () => (
+        <div className="w-full flex flex-col md:flex-row md:justify-between md:space-x-6 items-center">
+            {/* ===== Logo 部分 ===== */}
+            <div className="hidden md:flex w-16 h-auto rounded-full ">
+                <Link to="/">
+                    <img src="/img/Logo.png" alt="Logo" />
+                </Link>
+            </div>
+
+            <div className="md:flex items-center space-x-10">
+                {/* ===== 導覽列內容 ===== */}
+
+                <ul className="flex flex-col md:flex-row md:space-x-10">
+                    {navBarContent.map(({ to, label }) => (
+                        <li key={to}>
+                            <NavLink
+                                to={to}
+                                className={({ isActive }) =>
+                                    `text-base transition-all duration-300 ease-in-out ${isActive ? "opacity-100" : "opacity-60"
+                                    } hover:opacity-80`
+                                }
+                            >
+                                {label}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+
+
+                <div className='flex md:flex-row space-x-4'>
+                    { /* 用戶按鈕 */}
+                    <button className="hidden md:flex w-8 h-8 items-center">
+                        <CircleUserRound strokeWidth={2.5} className='w-10 h-auto' />
+                    </button>
+
+                    {/* 購物車按鈕 */}
+                    <button className="hidden md:flex w-8 h-8 items-center">
+                        <ShoppingCart strokeWidth={2.5} className='w-10 h-auto' />
+                    </button>
+                </div>
+                {/* 開關切換器 */}
+                <div className="md:block w-12 h-6 items-center">
+                    <input type="checkbox" value="synthwave" className="toggle theme-controller" />
+                </div>
+            </div>
+        </div>
+    )
+
+    return (
+        // 桌面板導覽列 
+        <>
+            {/* 桌面版導覽列 */}
+            <div className="hidden md:flex justify-between mt-4 mb-4 px-10 py-2 bg-white ">
+                <NavBarContent />
+            </div>
+
+            {/* 手機版 Drawer */}
+            <div className="drawer md:hidden w-full mt-4 mb-4 bg-white ">
+                <input id="drawer-toggle" type="checkbox" className="drawer-toggle" checked={isOpen} readOnly />
+                {/* 手機版Header */}
+                <div className="drawer-content z-999">
+                    <div className="w-full flex items-center justify-between px-10 py-2 ">
+                        <div className="flex items-center">
+                            <HamMenu
+                                onClick={() => setIsOpen(!isOpen)}
+                                isOpen={isOpen}
+                            />
+                        </div>
+                        <div className="flex items-center gap-4">
+                            <button className="w-8 h-8">
+                                <CircleUserRound strokeWidth={2.5} className="w-full h-full" />
+                            </button>
+                            <button className="w-8 h-8">
+                                <ShoppingCart strokeWidth={2.5} className="w-full h-full" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="drawer-side z-50">
+                    <div className={`fixed w-full left-0 right-0 transition-all duration-300 ease-in-out z-40 ${isOpen
+                        ? 'top-[84px] h-[calc(100vh-84px)]'
+                        : 'top-[-100vh]'
+                        }`}>
+                        <div className="menu w-full justify-center items-center drawer-bg">
+                            <NavBarContent />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NavBar;
