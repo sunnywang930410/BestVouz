6709d228ca057dac960382949a6c9dbf2c29d3a9
AMA0099112 <s111119022@stu.ntue.edu.tw>
2025-04-16 02:23:11 +0800

    修改一點點菜單頁排版

--- src/component/Productitem.jsx
+++ src/component/Productitem.jsx
@@ -3,20 +3,21 @@ import { Link } from "react-router";
 
 const ProductItem = ({ product }) => {
     return (
-
-
-        <div>
-            <div className="card card-side bg-base-100 shadow-sm">
-                <figure>
+        <div className="justify-center items-center">
+            <div className="card card-side !rounded-lg border-2 bg-base-100 ">
+                <figure className="w-90 h-auto">
                     <img
-                        className="w-70 h-auto"
+                        className="w-full h-full object-cover rounded-lg"
                         src={product.cover}
                         alt={product.name} />
                 </figure>
                 <div className="card-body">
-                    <p className="card-title">{product.name}</p>
-                    <p className="text-base">{product.description}</p>
-                    <p className="text-x1">{product.price}</p>
+                    <h3 className="card-title">{product.name}</h3>
+                    <h5 className="text-base">{product.description}</h5>
+
+                    <h4 className="text-x1">{product.price}</h4>
+
+
                     <div className="card-actions justify-end">
 
                         <Link to={`/product/${product.id}`}>
